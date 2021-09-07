6fac41b47dbd4d4df0a01b84507bca049507e44f
unknown <osyckaliam@gmail.com>
2021-09-07 11:16:52 -0300

    Conexion con BD PostgreSQL Local

--- transporte/gestionCargas/views.py
+++ transporte/gestionCargas/views.py
@@ -1,10 +1,18 @@
+from rest_framework import serializers
 from rest_framework.serializers import Serializer
+from rest_framework.response import Response
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
 from .serializers import SolicitudTransporteSerializer
 from .models import SolicitudTransporte
 
-class SolicitudTransporteView(generics.CreateAPIView):
-    queryset=SolicitudTransporte.objects.all()
+class SolicitudTransporteView(APIView):
     serializer_class=SolicitudTransporteSerializer
+
+    def get(self, request, format=None):
+        solicitudes=SolicitudTransporte.objects.all()
+        serializer=SolicitudTransporteSerializer(solicitudes, many=True)
+        return Response(serializer.data)
+
 
--- transporte/transporte/urls.py
+++ transporte/transporte/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gestionCargas', include('gestionCargas.urls')),
-    path('', include('frontend.urls'))
+    path('gestionCargas/', include('gestionCargas.urls')),
+    #path('', include('frontend.urls'))
 ]
