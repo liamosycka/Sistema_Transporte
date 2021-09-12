64f500bab157177f3d18a0c67ed42f1d2f0634d9
unknown <osyckaliam@gmail.com>
2021-09-12 00:59:18 -0300

    Nuevas views y urls

--- transporte/gestionCargas/urls.py
+++ transporte/gestionCargas/urls.py
@@ -1,6 +1,46 @@
 from django.urls import path
-from .views import SolicitudTransporteView
+from django.urls import re_path
+from rest_framework.routers import DefaultRouter
+from .views import ClienteViewSet, SolicitudListView, SolicitudTransporteFecha, SolicitudTransporteView
 
+"""
+
+router=DefaultRouter()
+router.register(r'clientes', ClienteViewSet, basename='clientes')
+urlpatterns=router.urls
+"""
+
+"""
+comentar lo de abajo para usar el router de cliente
+"""
 urlpatterns = [
-    path('home', SolicitudTransporteView.as_view())
+    path('sols/',SolicitudTransporteView.as_view()),
+    path('sols/<str:fecha>/<int:op>/', SolicitudTransporteFecha.as_view()),
+    path('sols/list/',SolicitudListView.as_view())
 ]
+
+
+
+
+"""
+Explicacion URLS:
+definimos el url:
+path('home/', some_view)
+La url:
+/home/?fecha=2021-09-09&destinatario=asd se dirigirá al some_view, ya que DJANGO 
+coloca los params dentro de un dicc llamado query_params, que será asi: 
+<QueryDict: {'fecha': ['2021-09-09'], 'destinatario': ['Pedro']}>
+entonces fecha estará ahi dentro, para accederlo desde some_view
+podemos hacer request.query_params['fecha']
+la view solo tiene que tener esto:
+def get(self, request)
+------------------------------------
+La manera que Django recomienda enviar params por el GET es asi:
+/home/2021-09-09/Pedro
+definiendo una ruta: 
+path('home/<str:fecha>/<str:destinatario>)
+y definiendo una vista:
+def get(self, request, fecha, destinatario)
+y accediendo los params solo por los atributos
+
+"""
--- transporte/gestionCargas/views.py
+++ transporte/gestionCargas/views.py
@@ -5,13 +5,24 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.views import APIView
-from .serializers import SolicitudTransporteSerializer
-from .models import SolicitudTransporte
+from rest_framework import viewsets
+from .serializers import ClienteSerializer, SolicitudTransporteSerializer
+from .models import SolicitudTransporte, Cliente
+
+class ClienteViewSet (viewsets.ModelViewSet):
+    """esto encapsula todas las operaciones CRUD básicas y con el router
+    creamos automáticamente todos los endpoints requeridos"""
+    serializer_class=ClienteSerializer
+    queryset=Cliente.objects.all()
+
+class SolicitudListView(generics.ListCreateAPIView):
+    """es lo mismo que la clase SolicitudTransporteView"""
+    queryset=SolicitudTransporte.objects.all()
+    serializer_class=SolicitudTransporteSerializer
 
 class SolicitudTransporteView(APIView):
-    #serializer_class=SolicitudTransporteSerializer
-   
-    def get(self, request, format=None):
+
+    def get(self, request,format=None):
         solicitudes=SolicitudTransporte.objects.all()
         serializer=SolicitudTransporteSerializer(solicitudes, many=True)
         return Response(serializer.data)
@@ -19,16 +30,24 @@ class SolicitudTransporteView(APIView):
     def post(self, request, format=None):
         serializer=SolicitudTransporteSerializer(data=request.data)
         if serializer.is_valid():
-            print("correcto: ")
-            print(serializer)
-            print("tipo: ",type(serializer))
             if (serializer.save()):
                 print("insertó")
             else:
                 print("no insertó")
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            print("incorrecto")
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+class SolicitudTransporteFecha(APIView):
+    def get(self, request, fecha, op,format=None):
+        """Si op=0 solicitud.fecha=fecha
+        Si op=1 solicitud.fecha>=fecha
+        Si op=2 solicitud.fecha<=fecha"""
+        if(op==0):
+            solicitud=SolicitudTransporte.objects.filter(fecha=fecha)
+        elif(op==1):
+            solicitud=SolicitudTransporte.objects.filter(fecha__gte=fecha)
+        elif(op==2):
+            solicitud=SolicitudTransporte.objects.filter(fecha__lte=fecha)
+        serializer=SolicitudTransporteSerializer(solicitud, many=True)
+        return Response(serializer.data)
